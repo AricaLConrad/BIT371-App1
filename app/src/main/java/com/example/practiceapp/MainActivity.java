99408a9fc7c10cb42ca59a4877c936596c499a93
AricaLConrad <AricaConrad@outlook.com>
2020-10-10 16:35:08 -0700

    Changed app to accept and check username and password fields.

--- app/src/main/java/com/example/practiceapp/MainActivity.java
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.practiceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,9 +17,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sayHello(View view) {
-        Log.i("INFO", "Hello Android!");
-        TextView tv = findViewById(R.id.textView);
-        tv.setText("Hello Android!");
+    // Go to the second activity when the button is pressed.
+    public void goToUserConfirmationActivity(View view) {
+
+        // Save whatever the user types into the username and password fields.
+        EditText username = findViewById(R.id.username);
+        Log.i("INFO", "Username: " + username.getText().toString());
+        EditText password = findViewById(R.id.password);
+        Log.i("INFO", "Password: " + password.getText().toString());
+
+        // Convert both username and password to a string.
+        String myUsername = username.getText().toString();
+        String myPassword = password.getText().toString();
+
+        // Start the intent, passing the two strings to the second activity.
+        Log.i("INFO", "Transitioning to " + UserConfirmationActivity.class.getName());
+        Intent intent = new Intent(getApplicationContext(), UserConfirmationActivity.class);
+        intent.putExtra("Username", myUsername);
+        intent.putExtra("Password", myPassword);
+        startActivity(intent);
     }
 }
